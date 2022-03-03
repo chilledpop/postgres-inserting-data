e46a972020a6d5bd9100c37ae597f8e90f933241
Thaomai Nguyen <thaomaicnguyen@gmail.com>
2022-03-03 14:59:32 -0800

    submitted

---
+++ src/artists/insert-multiple-artists.sql
@@ -0,0 +1,5 @@
+INSERT INTO artists (first_name, last_name, birthday, is_alive)
+VALUES
+  ('thaomai', 'nguyen', '1999-07-16', true),
+  ('peach', 'grany', '1995-02-15', true),
+  ('henry', 'lo', '1997-12-05', true);
